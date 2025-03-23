5bd12d4916727141c4def59ecc89f3e339795112
Khushv4 <khushalvyas21@gmail.com>
2025-03-24 03:21:15 +0530

    added Nav

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto bg-black bg-white-DEFAULT'>
+      <h1 className='text-8xl text-black-DEFAULT'> helloo</h1>
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
